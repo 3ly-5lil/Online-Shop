98cbc805be42d50a97a1ede06b14268fd5eb6ae1
Ali Khalil <79375840+3ly-5lil@users.noreply.github.com>
2025-12-12 18:37:33 +0200

    feat: created product service - created post, get operations for creating products and getting all product - connect the application to mongodb but there is problem when authentication is required - added integration test using test container

---
+++ product-service/src/main/java/com/ak/online_shop/product_service/service/ProductService.java
@@ -0,0 +1,47 @@
+package com.ak.online_shop.product_service.service;
+
+import com.ak.online_shop.product_service.dto.ProductRequest;
+import com.ak.online_shop.product_service.dto.ProductResponse;
+import com.ak.online_shop.product_service.model.Product;
+import com.ak.online_shop.product_service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .id(productRequest.id())
+                .name(productRequest.name())
+                .price(productRequest.price())
+                .description(productRequest.description())
+                .build();
+
+        productRepository.save(product);
+
+        log.info("product saved successfully");
+
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll().stream().map(
+                product -> new ProductResponse(
+                        product.getId(),
+                        product.getName(),
+                        product.getDescription(),
+                        product.getPrice())
+        ).toList();
+    }
+}
--- product-service/src/test/java/com/ak/online_shop/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/ak/online_shop/product_service/ProductServiceApplicationTests.java
@@ -1,15 +1,53 @@
 package com.ak.online_shop.product_service;
 
+import com.ak.online_shop.product_service.dto.ProductRequest;
+import io.restassured.RestAssured;
+import lombok.RequiredArgsConstructor;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.mongodb.MongoDBContainer;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.math.BigDecimal;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
+    @LocalServerPort
+    private Integer port;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
 
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws JsonProcessingException {
+        ProductRequest productRequest = new ProductRequest(
+                null,
+                "product 1",
+                "description 1",
+                new BigDecimal("100.015")
+        );
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(mapper.writeValueAsString(productRequest))
+                .when()
+                .post("api/product")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo(productRequest.name()));
 	}
 
 }
