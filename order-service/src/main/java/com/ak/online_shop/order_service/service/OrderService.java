4d1d3c3d81fe0906d715f2ae0788680e6b0bb194
Ali Khalil <79375840+3ly-5lil@users.noreply.github.com>
2025-12-16 05:12:11 +0200

    feat: connected order service to inventory service - used spring cloud openfeign to connect the services using the url - added isInStock to check product availability before submitting the order - changed the port the database run in each container to resolve conflict - the integration test to be done

--- order-service/src/main/java/com/ak/online_shop/order_service/service/OrderService.java
+++ order-service/src/main/java/com/ak/online_shop/order_service/service/OrderService.java
@@ -1,19 +1,29 @@
 package com.ak.online_shop.order_service.service;
 
+import com.ak.online_shop.order_service.client.InventoryClient;
 import com.ak.online_shop.order_service.dto.OrderRequest;
 import com.ak.online_shop.order_service.model.Order;
 import com.ak.online_shop.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public String createOrder(OrderRequest orderRequest) {
+        log.info(orderRequest.toString());
+
+        boolean isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (!isInStock) throw new RuntimeException("Product " + orderRequest.skuCode() + " isn't in stock");
+
         Order order = Order.builder()
                 .skuCode(orderRequest.skuCode())
                 .price(orderRequest.price())
--- order-service/src/test/java/com/ak/online_shop/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/ak/online_shop/order_service/OrderServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.ak.online_shop.order_service;
 
 import com.ak.online_shop.order_service.dto.OrderRequest;
+import com.ak.online_shop.order_service.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+//import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -15,6 +17,8 @@ import java.math.BigDecimal;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// TODO: implement the connection test with wire mock using stub instead of testing on direct connection
+//@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
     @LocalServerPort
     private Integer port;
@@ -31,10 +35,12 @@ class OrderServiceApplicationTests {
         OrderRequest orderRequest = new OrderRequest(
                 null,
                 null,
-                "product",
+                "product_1",
                 new BigDecimal("500.25"),
                 2
         );
+
+        InventoryClientStub.stubInventoryCall(orderRequest.skuCode(), orderRequest.quantity());
 
         RestAssured.given()
                 .contentType("application/json")
