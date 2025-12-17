b28cf4affdd3132807e310e782b388ae13fd4910
Ali Khalil <79375840+3ly-5lil@users.noreply.github.com>
2025-12-17 03:10:32 +0200

    created routing to the services in the api gateway

---
+++ api-gateway/src/main/java/com/ak/online_shop/api_gateway/routes/Routes.java
@@ -0,0 +1,35 @@
+package com.ak.online_shop.api_gateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.*;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@Configuration
+public class Routes {
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute() {
+        return GatewayRouterFunctions.route("product-service")
+                .route(RequestPredicates.path("/api/product"), http())
+                .before(uri("http://localhost:8080"))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+        return GatewayRouterFunctions.route("order-service")
+                .route(RequestPredicates.path("/api/order"), http())
+                .before(uri("http://localhost:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return GatewayRouterFunctions.route("inventory-service")
+                .route(RequestPredicates.path("/api/inventory"), http())
+                .before(uri("http://localhost:8082"))
+                .build();
+    }
+}
